db3b9fa4d8dcc9f5ee87d3d06e9a9e989508da88
Nikolai Aristov <KodiStudios@gmail.com>
2021-03-20 15:38:06 -0700

    Add Command Line Parsing Logic
    Add Test Placeholder

--- MidiCppConsole/MidiCppConsole.cpp
+++ MidiCppConsole/MidiCppConsole.cpp
@@ -16,8 +16,6 @@
 #include <iostream>
 #include <thread>
 
-using namespace std::chrono_literals;
-
 // Midi message is 4 bytes.
 // Windows Midi midiOutShortMsg() Api passes
 // those 4 bytes as DWORD type.
@@ -108,7 +106,12 @@ void SelectMidiInstrument(
 	midiOutShortMsg(hMidiOut, midiMessage.dwData);
 }
 
-void PlayNote()
+void PlayNote(
+	uint8_t channel,
+	uint8_t instrument,
+	uint8_t pitch, // Note
+	uint8_t velocity, // Volume
+	uint32_t noteLength)
 {
 	HMIDIOUT hMidiOut;
 	midiOutOpen(
@@ -119,13 +122,13 @@ void PlayNote()
 		/*fdwOpen*/ CALLBACK_NULL
 	);
 
-	// Select Grand Piano Instrument for Channel 0
-	SelectMidiInstrument(hMidiOut, /*channel*/ 0, /*Grand Piano*/ 0);
+	// Select Instrument for given Channel
+	SelectMidiInstrument(hMidiOut, channel, instrument);
 
-	std::cout << "Play Piano C Note\n";
-	SendMidiNote(hMidiOut, /*channel*/ 0, /*note*/ 60, /*velocity*/ 127);
-	std::this_thread::sleep_for(3s);
-	SendMidiNote(hMidiOut, /*channel*/ 0, /*note*/ 60, /*velocity*/ 0); // Stop
+	// Start Playing Note
+	SendMidiNote(hMidiOut, channel, pitch, velocity);
+	std::this_thread::sleep_for(std::chrono::milliseconds(noteLength));
+	SendMidiNote(hMidiOut, channel, pitch, 0); // Stop
 
 	midiOutClose(hMidiOut);
 }
@@ -209,7 +212,12 @@ namespace Robust
 		VerifyMidi(midiOutShortMsg(hMidiOut, midiMessage.dwData));
 	}
 
-	void PlayNote()
+	void PlayNote(
+		uint8_t channel,
+		uint8_t instrument,
+		uint8_t pitch, // Note
+		uint8_t velocity, // Volume
+		uint32_t noteLength)
 	{
 		try
 		{
@@ -222,13 +230,13 @@ namespace Robust
 				/*fdwOpen*/ CALLBACK_NULL
 			));
 
-			// Set Instrument for Channel 0
-			Robust::SelectMidiInstrument(hMidiOut, /*channel*/ 0, /*Grand Piano*/ 0);
+			// Select Instrument for Channel 0
+			Robust::SelectMidiInstrument(hMidiOut, /*channel*/ channel, /*Grand Piano*/ instrument);
 
-			std::cout << "Play Piano C Note\n";
-			Robust::SendMidiNote(hMidiOut, /*channel*/ 0, /*note*/ 60, /*velocity*/ 127);
-			std::this_thread::sleep_for(3s);
-			Robust::SendMidiNote(hMidiOut, /*channel*/ 0, /*note*/ 60, /*velocity*/ 0); // Stop
+			// Start Playing Note
+			Robust::SendMidiNote(hMidiOut, channel, pitch, velocity);
+			std::this_thread::sleep_for(std::chrono::milliseconds(noteLength));
+			Robust::SendMidiNote(hMidiOut, channel, pitch, 0); // Stop
 
 			VerifyMidi(midiOutClose(hMidiOut));
 		}
@@ -240,9 +248,105 @@ namespace Robust
 	}
 }
 
-int main()
+class AppArguments
 {
-	Robust::PlayNote();
-	PlayNote();
+public:
+	uint8_t Channel{ 0 };
+	uint8_t Instrument{ 0 };       // 0 is Grand Piano
+	uint8_t Pitch{ 60 };           // 60 is C Note
+	uint8_t Velocity{ 127 };       // 127 is Max Velocity (Volume)
+	uint32_t Length{ 3000 };       // Note Length, in Milliseconds
+	bool SimpleApi{ false };       // Use Simple Midi Api, without error detection
+	bool UserOverridden{ false };  // User had specified some flags
+	bool Help{ false };
+};
+
+namespace ArgumentParsing {
+	template<class T>
+	T GetValue(char* v)
+	{
+		return (T)atoi(v);
+	}
+
+	std::unique_ptr<AppArguments> ParseArguments(int argc, char* argv[])
+	{
+		std::unique_ptr<AppArguments> myArguments{ std::make_unique<AppArguments>() };
+		
+		// argv[0] is the name of the Program
+		// argv[1] is the first argument
+		for (int i = 1; i < argc; i++)
+		{
+			myArguments->UserOverridden = true;
+			if (!strcmp(argv[i], "-c") || !strcmp(argv[i], "/c")) { i++; Robust::VerifyLimit(i, argc - 1, "Channel"); myArguments->Channel = GetValue<uint8_t>(argv[i]); }
+			if (!strcmp(argv[i], "-i") || !strcmp(argv[i], "/i")) { i++; Robust::VerifyLimit(i, argc - 1, "Instrument"); myArguments->Instrument = GetValue<uint8_t>(argv[i]); }
+			if (!strcmp(argv[i], "-p") || !strcmp(argv[i], "/p")) { i++; Robust::VerifyLimit(i, argc - 1, "Pitch"); myArguments->Pitch = GetValue<uint8_t>(argv[i]); }
+			if (!strcmp(argv[i], "-v") || !strcmp(argv[i], "/v")) { i++; Robust::VerifyLimit(i, argc - 1, "Velocity"); myArguments->Velocity = GetValue<uint8_t>(argv[i]); }
+			if (!strcmp(argv[i], "-l") || !strcmp(argv[i], "/l")) { i++; Robust::VerifyLimit(i, argc - 1, "Length"); myArguments->Length = GetValue<uint32_t>(argv[i]); }
+			if (!strcmp(argv[i], "-s") || !strcmp(argv[i], "/s")) { myArguments->SimpleApi = true; }
+			if (!strcmp(argv[i], "-?") || !strcmp(argv[i], "/?")) { myArguments->Help = true; }
+		}
+
+		return myArguments;
+	}
+
+	void PrintHelp(char* appName)
+	{
+		AppArguments defaultArguments;
+		std::cout << "All flags are Optional!\n";
+		std::cout << "-c [0-15]" << "     Channel. Default: " << defaultArguments.Channel << "\n";
+		std::cout << "-i [0-127]" << "    Instrument. Default: " << defaultArguments.Instrument << "(Grand Piano)\n";
+		std::cout << "-p [0-127]" << "    Pitch (Note). Default: " << defaultArguments.Pitch << "(Middle C Note)\n";
+		std::cout << "-v [0-127]" << "    Velocity (Volume). Default: " << defaultArguments.Velocity << "\n";
+		std::cout << "-l [milliseconds]" << "   Length (Note Length), in Milliseconds. Default: " << defaultArguments.Length << "(3 Seconds)\n";
+		std::cout << "-s" << "            Use Simple Midi Api, no error detection.\n";
+		std::cout << "-?" << "            Print this help\n";
+		std::cout << "\n";
+		std::cout << "Sample Usage:\n";
+		std::cout << "\n";
+		std::cout << appName << "-i 24 -p 80\n";
+		std::cout << "Play Guitar Note\n";
+		std::cout << "\n";
+		std::cout << appName << "-c 1 -i 24 -p 81 -v 120 -l 2000\n";
+		std::cout << "Sets Guitar to Channel 1, Plays G Note, at Volume 120, for 2 seconds\n";
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	std::unique_ptr<AppArguments> appArguments;
+	try
+	{
+		appArguments = ArgumentParsing::ParseArguments(argc, argv);
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "Incorrect Arguments\n";
+		std::cout << "Exception: " << e.what() << "\n";
+		ArgumentParsing::PrintHelp(/*appName*/ argv[0]);
+		throw;
+	}
+
+	if (appArguments->Help)
+	{
+		ArgumentParsing::PrintHelp(/*appName*/ argv[0]);
+		return 0;
+	}
+
+	if (!appArguments->UserOverridden)
+	{
+		std::cout << "Play Piano C Note\n";
+	}
+
+	if (appArguments->SimpleApi)
+	{
+		// No error checks
+		PlayNote(appArguments->Channel, appArguments->Instrument, appArguments->Pitch, appArguments->Velocity, appArguments->Length);
+	}
+	else
+	{
+		// With error checks
+		Robust::PlayNote(appArguments->Channel, appArguments->Instrument, appArguments->Pitch, appArguments->Velocity, appArguments->Length);
+	}
+	
 	return 0;
 }
