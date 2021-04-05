702586516f5a57572f36a165593edbf70c3b795f
Nikolai Aristov <KodiStudios@gmail.com>
2021-04-04 18:11:12 -0700

    Minimize Code

---
+++ MidiCppConsoleTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MidiCppConsoleTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MidiCppConsoleTest")]
+[assembly: AssemblyCopyright("Copyright © Kodi Studios 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("79b8db1b-ae79-44d6-8e5c-591f39ca0630")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
